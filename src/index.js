e2060017b2f9336f6ebfc842c031ecd402e88b1d
Ulises <ulises.rogo@gmail.com>
2020-05-07 19:01:36 -0500

    12. Crear un paquete para NPM

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const messages = ['Oscar', 'Ana', 'Nicolay', 'Yessica', 'Diego', 'Laura'];
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log(message);
+}
+
+module.exports = { randomMsg };
